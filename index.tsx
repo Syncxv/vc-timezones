8aeb03210eaf6d9e8056ae2a59bfdb8795b83244
Syncxv <aruldevarackal1@gmail.com>
2024-03-10 13:27:33 +1100

    fix contenxt menu

--- index.tsx
+++ index.tsx
@@ -178,7 +178,7 @@ export default definePlugin({
 });
 
 
-const userContextMenuPatch: NavContextMenuPatchCallback = (children, { user }: { user: User; }) => () => {
+const userContextMenuPatch: NavContextMenuPatchCallback = (children, { user }: { user: User; }) => {
     if (user?.id == null) return;
 
     const setTimezoneItem = (
